88974c15b4e32057385b89030f60cc79752c13d5
lanng <lannguyen0611@gmail.com>
2019-09-25 10:03:56 +0700

    add homepage

---
+++ app/main.py
@@ -0,0 +1,19 @@
+import firebase_admin
+from flask import Flask
+
+from blueprints import *
+
+
+# Initialize Firebase Admin SDK.
+# See https://firebase.google.com/docs/admin/setup for more information.
+firebase = firebase_admin.initialize_app()
+
+app = Flask(__name__)
+app.secret_key = b'A Super Secret Key'
+
+
+app.register_blueprint(home_page)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ app/static/js/initFirebase.js
@@ -0,0 +1,13 @@
+// Script for configuring Firebase.
+// See https://firebase.google.com/docs/web/setup for more information.
+
+var config = {
+    apiKey: "AIzaSyDu3CtZ-Z1u_uUUmd0mRt8EH9hto8BRYbc",
+    authDomain: "skincare-recommendation.firebaseapp.com",
+    databaseURL: "https://skincare-recommendation.firebaseio.com",
+    projectId: "skincare-recommendation",
+    storageBucket: "skincare-recommendation.appspot.com",
+    messagingSenderId: "211295146865"
+};
+
+firebase.initializeApp(config);
