9e3f0a4a85c2bdae05b96f6797a21e5104bf6752
lannguyen1106 <53172052+lannguyen1106@users.noreply.github.com>
2019-10-16 13:54:07 +0700

    Update blueprint.py

--- app/blueprints/home/blueprint.py
+++ app/blueprints/home/blueprint.py
@@ -1,13 +1,9 @@
-# app/blueprints/home/blueprint.py
 """
 This module is the Flask blueprint for the product catalog page (/).
 """
 
 
 from flask import Blueprint, render_template
-
-# from models import product_catalog
-# from middlewares.auth import auth_optional
 
 home_page = Blueprint('home_page', __name__)
 
